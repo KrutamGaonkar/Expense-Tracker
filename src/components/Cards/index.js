1be30dc6d1ac1d11cdcaf4ecc3cdf8fb523697a8
Krutam <krutam103@gmail.com>
2024-08-31 17:21:58 +0530

    making responsive

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -12,11 +12,11 @@ function Cards({ setIsIncomeModalVisible, setIsExpenseModalVisible, setIsDeleteM
                     <p className='my-p'>₹{balance}</p>
                     <Button disable={isDisabled} text={"Reset Balance"} blue={true} onClick={()=>{setIsDeleteModalVisible(true)}}/>
                 </Card>
-                <Card className='my-card' title="Total Income">
+                <Card className='my-card no-top-margin' title="Total Income">
                     <p className='my-p'>₹{income}</p>
                     <Button disable={false} text={"Add Income"} blue={true} onClick={()=>{setIsIncomeModalVisible(true)}}/>
                 </Card>
-                <Card className='my-card' title="Total Expense">
+                <Card className='my-card no-top-margin' title="Total Expense">
                     <p className='my-p'>₹{expense}</p>
                     <Button disable={false} text={"Add Expense"} blue={true} onClick={()=>{setIsExpenseModalVisible(true)}}/>
                 </Card>
